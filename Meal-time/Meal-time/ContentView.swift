9e6b7edefeb5cdc3fb5ceec8c2bcb7375e0cb4ff
yoonhyeokCho <hyeok6640@gmail.com>
2023-03-13 19:25:28 +0900

    init

---
+++ Meal-time/Meal-time/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Meal-time
+//
+//  Created by yoonhyeok on 2023/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Meal-time/Meal-time/Meal_timeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_timeApp.swift
+//  Meal-time
+//
+//  Created by yoonhyeok on 2023/03/13.
+//
+
+import SwiftUI
+
+@main
+struct Meal_timeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
